43a4d55b11f5bacd7e6f00108751ee7f789bc4b2
Lizzy <0572788@gmail.com>
2017-03-12 23:41:39 -0700

    created creat/update/delete for all controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,36 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def create
-    user = User.new(
+    @user = User.new(
       name: params[:name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
-    if user.save
-      session[:user_id] = user.id
-      flash[:success] = 'Successfully created account!'
-      redirect_to '/'
+    if @user.save
+      render :nothing => true, :status => 200
     else
-      flash[:warning] = 'Invalid email or password!'
-      redirect_to '/signup'
+      render :nothing => true, :status => 400
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if @user.save
+      render :nothing => true, :status => 200
+    else
+      render :nothing => true, :status => 400
+    end
+  end
+
+  def destroy
+   @user = User.find(params[:id])
+   @user.destroy
+   render :nothing => true, :status => 200
   end
 end
