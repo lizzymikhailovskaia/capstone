34fe2777332ae0d5c52cc426541948aee28ba5aa
Lizzy <0572788@gmail.com>
2017-06-10 20:35:23 -0700

    serve client side in rails

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: __dirname,
+    path: `${__dirname}/../app/assets/webpack`,
     publicPath: '/',
     filename: 'bundle.js'
   },
